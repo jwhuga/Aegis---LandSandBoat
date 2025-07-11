a1cb23b636fbaed29163f3fa6ce89dd73f9b5e58
jwhuga <44433768+jwhuga@users.noreply.github.com>
2025-07-11 18:00:20 -0400

    Update night_terror.lua
    removed extra effects to make sure its functional.  Need to add 40% increase contingent on sleep effect once functional.

--- scripts/actions/abilities/pets/night_terror.lua
+++ scripts/actions/abilities/pets/night_terror.lua
@@ -1,10 +1,7 @@
 -----------------------------------
 -- Night Terror
--- Description: Deals Darkness-element magical damage (AoE). 
--- Characteristics:
--- * Does 40% more damage to targets that are asleep.
--- * Frequently used solo or against high-stat monsters.
--- Note: Created by Caeda on 2025-07-11
+-- Description: Deals Darkness-element magical damage (AoE).
+-- Note: Created by Caeda on 2025-07-09
 -----------------------------------
 ---@type TAbilityPet
 local abilityObject = {}
@@ -16,17 +13,10 @@ end
 abilityObject.onPetAbility = function(target, pet, petskill, summoner, action)
     xi.job_utils.summoner.onUseBloodPact(target, petskill, summoner, action)
 
-    local baseDamage = 3000 + (pet:getStat(xi.mod.INT) - target:getStat(xi.mod.INT))
-    local multiplier = 1.0
+    local baseDmg = 4.0
+    local damage = 3000 + (pet:getStat(xi.mod.INT) - target:getStat(xi.mod.INT))
 
-    -- Increase damage by 40% if target is asleep
-    if target:hasStatusEffect(xi.effect.SLEEP) or target:hasStatusEffect(xi.effect.SLEEP_II) then
-        multiplier = 1.4
-    end
-
-    local damage = baseDamage * multiplier
-
-    damage = xi.mobskills.mobMagicalMove(pet, target, petskill, damage, xi.element.DARK, 4.0, xi.mobskills.magicalTpBonus.NO_EFFECT, 0)
+    damage = xi.mobskills.mobMagicalMove(pet, target, petskill, damage, xi.element.DARK, baseDmg, xi.mobskills.magicalTpBonus.NO_EFFECT, 0)
     damage = xi.mobskills.mobAddBonuses(pet, target, damage, xi.element.DARK, petskill)
     damage = xi.summon.avatarFinalAdjustments(damage, pet, petskill, target, xi.attackType.MAGICAL, xi.damageType.DARK, 1)
 
