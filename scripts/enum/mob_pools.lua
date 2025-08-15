71412e6c368963bb1fbb42a32f5dfcc6b28b5b48
WinterSolstice8 <60417494+wintersolstice8@users.noreply.github.com>
2025-08-14 20:57:59 -0600

    [lua] Add mob_pools enum in lua

---
+++ scripts/enum/mob_pools.lua
@@ -0,0 +1,12 @@
+-----------------------------------
+-- Mob pools, same as the pools in mob_pools.sql
+-- Usually used for some custom mobskill effects for NMs
+-----------------------------------
+xi = xi or {}
+
+---@enum xi.mobPools
+xi.mobPools =
+{
+    PEPPER  = 3116, -- BCNM20 Charming Trio, Absorbing Kiss
+    PHOEDME = 3132, -- BCNM20 Charming Trio, Deep Kiss
+}
