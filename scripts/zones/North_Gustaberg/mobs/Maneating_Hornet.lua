45779f0405679ccc184319aef977edb0bb3f4e8f
MowFord <131182600+MowFord@users.noreply.github.com>
2025-08-23 13:53:13 -0500

    Updated phList/spawnPoints to nm lua: M-N* zones

--- scripts/zones/North_Gustaberg/mobs/Maneating_Hornet.lua
+++ scripts/zones/North_Gustaberg/mobs/Maneating_Hornet.lua
@@ -8,22 +8,6 @@ local ID = zones[xi.zone.NORTH_GUSTABERG]
 ---@type TMobEntity
 local entity = {}
 
-local sophiePHTable =
-{
-    [ID.mob.STINGING_SOPHIE[1] - 6] = ID.mob.STINGING_SOPHIE[1], -- 196.873 -40.415 500.153
-    [ID.mob.STINGING_SOPHIE[1] - 5] = ID.mob.STINGING_SOPHIE[1], -- 352.974 -40.359 472.914
-    [ID.mob.STINGING_SOPHIE[1] - 4] = ID.mob.STINGING_SOPHIE[1], -- 216.150 -41.182 445.157
-    [ID.mob.STINGING_SOPHIE[1] - 3] = ID.mob.STINGING_SOPHIE[1], -- 353.313 -40.347 463.609
-    [ID.mob.STINGING_SOPHIE[1] - 2] = ID.mob.STINGING_SOPHIE[1], -- 237.753 -40.500 469.738
-    [ID.mob.STINGING_SOPHIE[1] - 1] = ID.mob.STINGING_SOPHIE[1], -- 197.369 -40.612 453.688
-
-    [ID.mob.STINGING_SOPHIE[2] - 5] = ID.mob.STINGING_SOPHIE[2], -- 210.607 -40.478 566.096
-    [ID.mob.STINGING_SOPHIE[2] - 4] = ID.mob.STINGING_SOPHIE[2], -- 288.447 -40.842 634.161
-    [ID.mob.STINGING_SOPHIE[2] - 3] = ID.mob.STINGING_SOPHIE[2], -- 295.890 -41.593 614.738
-    [ID.mob.STINGING_SOPHIE[2] - 2] = ID.mob.STINGING_SOPHIE[2], -- 363.973 -40.774 562.355
-    [ID.mob.STINGING_SOPHIE[2] - 1] = ID.mob.STINGING_SOPHIE[2], -- 356.544 -40.528 570.302
-}
-
 local stingingSophie =
 {
     [ID.mob.STINGING_SOPHIE[1]] =
@@ -90,10 +74,9 @@ entity.onMobDeath = function(mob, player, optParams)
 end
 
 entity.onMobDespawn = function(mob)
-    local nmId   = sophiePHTable[mob:getID()]
+    local nmId   = DELETEME[mob:getID()]
     local params = { }
-    params.spawnPoints = stingingSophie[nmId]
-    xi.mob.phOnDespawn(mob, sophiePHTable, 5, 1, params) -- Pure Lottery
+    xi.mob.phOnDespawn(mob, ID.mob.STINGING_SOPHIE[1], 5, 1, params) -- Pure Lottery
 end
 
 return entity
