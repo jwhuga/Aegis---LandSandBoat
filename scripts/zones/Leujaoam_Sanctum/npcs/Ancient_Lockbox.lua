8954def4553211710d6a367fb51fe06b4409cf75
MowFord <131182600+MowFord@users.noreply.github.com>
2025-09-19 10:29:58 -0500

    update assaults to use lootGroup helper

--- scripts/zones/Leujaoam_Sanctum/npcs/Ancient_Lockbox.lua
+++ scripts/zones/Leujaoam_Sanctum/npcs/Ancient_Lockbox.lua
@@ -11,17 +11,17 @@ entity.onTrigger = function(player, npc)
         [xi.assault.mission.LEUJAOAM_CLEANSING] =
         {
             {
-                { itemid = xi.item.UNAPPRAISED_RING, droprate = 700 },
-                { itemid = xi.item.UNAPPRAISED_BOX,  droprate = 300 },
+                { item = xi.item.UNAPPRAISED_RING, weight = 700 },
+                { item = xi.item.UNAPPRAISED_BOX,  weight = 300 },
             },
         },
 
         [xi.assault.mission.ORICHALCUM_SURVEY] =
         {
             {
-                { itemid = xi.item.UNAPPRAISED_NECKLACE, droprate = 300 },
-                { itemid = xi.item.UNAPPRAISED_BOX,      droprate = 400 },
-                { itemid = xi.item.UNAPPRAISED_GLOVES,   droprate = 300 },
+                { item = xi.item.UNAPPRAISED_NECKLACE, weight = 300 },
+                { item = xi.item.UNAPPRAISED_BOX,      weight = 400 },
+                { item = xi.item.UNAPPRAISED_GLOVES,   weight = 300 },
             },
         },
     }
@@ -31,29 +31,29 @@ entity.onTrigger = function(player, npc)
         [xi.assault.mission.LEUJAOAM_CLEANSING] =
         {
             {
-                { itemid = xi.item.HI_POTION_P3, droprate = 1000 },
+                { item = xi.item.HI_POTION_P3, weight = 1000 },
             },
 
             {
-                { itemid = xi.item.HI_POTION_P3, droprate = 100 },
-                { itemid = 0,                    droprate = 900 },
+                { item = xi.item.HI_POTION_P3, weight = 100 },
+                { item = 0,                    weight = 900 },
             },
 
             {
-                { itemid = xi.item.REMEDY, droprate = 530 },
-                { itemid = 0,               droprate = 470 },
+                { item = xi.item.REMEDY, weight = 530 },
+                { item = 0,               weight = 470 },
             },
         },
 
         [xi.assault.mission.ORICHALCUM_SURVEY] =
         {
             {
-                { itemid = xi.item.HI_POTION_P3, droprate = 1000 },
+                { item = xi.item.HI_POTION_P3, weight = 1000 },
             },
 
             {
-                { itemid = xi.item.REMEDY, droprate = 530 },
-                { itemid = 0,               droprate = 470 },
+                { item = xi.item.REMEDY, weight = 530 },
+                { item = 0,               weight = 470 },
             },
         },
     }
