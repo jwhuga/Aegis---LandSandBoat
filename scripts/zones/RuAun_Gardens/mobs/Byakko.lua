cf9e13df21e751da29295130c478371d53ac91dc
Critical <48370698+CriticalXI@users.noreply.github.com>
2025-09-28 19:06:40 -0600

    [lua, sql] Byakko sky god audit

--- scripts/zones/RuAun_Gardens/mobs/Byakko.lua
+++ scripts/zones/RuAun_Gardens/mobs/Byakko.lua
@@ -8,15 +8,27 @@ mixins = { require('scripts/mixins/job_special') }
 ---@type TMobEntity
 local entity = {}
 
-entity.onMobSpawn = function(mob)
-    GetNPCByID(ID.npc.PORTAL_OFFSET + 8):setAnimation(xi.anim.CLOSE_DOOR)
-end
-
 entity.onMobInitialize = function(mob)
     mob:setMobMod(xi.mobMod.ADD_EFFECT, 1)
     mob:setMobMod(xi.mobMod.IDLE_DESPAWN, 300)
-    mob:setMobMod(xi.mobMod.GIL_MIN, 1500)
-    mob:setMobMod(xi.mobMod.GIL_MAX, 30000)
+    mob:setMobMod(xi.mobMod.GIL_MIN, 18000)
+    mob:setMobMod(xi.mobMod.GIL_MAX, 18000)
+    mob:setMobMod(xi.mobMod.WEAPON_BONUS, 32)
+    mob:setMod(xi.mod.DOUBLE_ATTACK, 10)
+    mob:setMod(xi.mod.VIT, 125)
+end
+
+entity.onMobSpawn = function(mob)
+    mob:messageText(mob, ID.text.SKY_GOD_OFFSET + 11) -- Spawn message
+    GetNPCByID(ID.npc.PORTAL_OFFSET + 8):setAnimation(xi.anim.CLOSE_DOOR)
+
+    -- Sky gods wait 10 seconds after spawning to start casting
+    mob:setMagicCastingEnabled(false)
+    mob:timer(10000, function(mobArg)
+        if mobArg then
+            mobArg:setMagicCastingEnabled(true)
+        end
+    end)
 end
 
 entity.onAdditionalEffect = function(mob, target, damage)
