03e316deba8683e9be484907b913273e1b0920af
Xaver-DaRed <xaver.entropia@gmail.com>
2025-10-02 14:30:59 +0200

    Move special treasure chest logic to quest scripts

--- scripts/zones/Crawlers_Nest/npcs/Treasure_Chest.lua
+++ scripts/zones/Crawlers_Nest/npcs/Treasure_Chest.lua
@@ -8,7 +8,15 @@
 local entity = {}
 
 entity.onTrade = function(player, npc, trade)
-    xi.treasure.onTrade(player, npc, trade, 0, 0)
+    if
+        xi.quest.getVar(player, xi.questLog.SANDORIA, xi.quest.id.sandoria.ENVELOPED_IN_DARKNESS, 'Prog') >= 2 and
+        xi.quest.getVar(player, xi.questLog.SANDORIA, xi.quest.id.sandoria.ENVELOPED_IN_DARKNESS, 'Time') == 0 and
+        not player:hasKeyItem(xi.keyItem.CRAWLER_BLOOD)
+    then
+        xi.treasure.onTrade(player, npc, trade, 2, xi.keyItem.CRAWLER_BLOOD)
+    else
+        xi.treasure.onTrade(player, npc, trade, 0, 0)
+    end
 end
 
 entity.onTrigger = function(player, npc)
--- scripts/zones/FeiYin/npcs/Treasure_Chest.lua
+++ scripts/zones/FeiYin/npcs/Treasure_Chest.lua
@@ -7,7 +7,14 @@
 local entity = {}
 
 entity.onTrade = function(player, npc, trade)
-    xi.treasure.onTrade(player, npc, trade, 0, 0)
+    if
+        player:getQuestStatus(xi.questLog.SANDORIA, xi.quest.id.sandoria.SORCERY_OF_THE_NORTH) == xi.questStatus.QUEST_ACCEPTED and
+        not player:hasKeyItem(xi.keyItem.FEIYIN_MAGIC_TOME)
+    then
+        xi.treasure.onTrade(player, npc, trade, 2, xi.keyItem.FEIYIN_MAGIC_TOME)
+    else
+        xi.treasure.onTrade(player, npc, trade, 0, 0)
+    end
 end
 
 entity.onTrigger = function(player, npc)
