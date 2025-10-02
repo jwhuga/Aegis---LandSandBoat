c7ecdc18de598d5856f5ffa83e3f790a6da40a71
sruon <sruon@users.noreply.github.com>
2025-10-01 20:10:39 -0600

    GP_SERV_COMMAND_JUMP
    Co-Authored-By: atom0s <atom0s@users.noreply.github.com>

--- src/map/packets/s2c/0x11e_jump.cpp
+++ src/map/packets/s2c/0x11e_jump.cpp
@@ -1,31 +1,31 @@
-﻿/*
+/*
 ===========================================================================
-  Copyright (c) 2021 Ixion Dev Teams
+
+  Copyright (c) 2025 LandSandBoat Dev Teams
+
   This program is free software: you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
   the Free Software Foundation, either version 3 of the License, or
   (at your option) any later version.
+
   This program is distributed in the hope that it will be useful,
   but WITHOUT ANY WARRANTY; without even the implied warranty of
   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
   GNU General Public License for more details.
+
   You should have received a copy of the GNU General Public License
   along with this program.  If not, see http://www.gnu.org/licenses/
+
 ===========================================================================
 */
 
-#ifndef _CCHAREMOTIONJUMPPACKET_H
-#define _CCHAREMOTIONJUMPPACKET_H
+#include "0x11e_jump.h"
 
-#include "common/cbasetypes.h"
+#include "entities/charentity.h"
 
-#include "basic.h"
-
-class CCharEntity;
-class CCharEmotionJumpPacket : public CBasicPacket
+GP_SERV_COMMAND_JUMP::GP_SERV_COMMAND_JUMP(CCharEntity* PChar, uint16_t targetIndex)
 {
-public:
-    CCharEmotionJumpPacket(CCharEntity* PChar, uint16 targetIndex);
-};
+    auto& packet = this->data();
 
-#endif
+    packet.ActIndex = targetIndex;
+}
